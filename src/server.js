b64d57a014e87bd1a041c9262e4036da276fadb0
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-05-03 17:45:36 +0200

    changed to live enviroment

--- src/server.js
+++ src/server.js
@@ -150,7 +150,7 @@ app
 
 
 mongoose
-  .connect(process.env.localDB, {
+  .connect(process.env.dbUrl, {
     connectWithNoPrimary: true,
       useCreateIndex: true,
       // useUnifiedTopology: true,
